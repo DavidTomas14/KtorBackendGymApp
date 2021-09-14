bb565c18516fbaff03c7302d2de2a9902afb1e6a
David Tomas <dtalonsocalderon@gmail.com>
2021-09-14 12:48:39 +0200

    Initial commit

---
+++ src/main/kotlin/com/dtalonso/Application.kt
@@ -0,0 +1,16 @@
+package com.dtalonso
+
+import io.ktor.application.*
+import com.dtalonso.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSockets()
+    configureSerialization()
+    configureHTTP()
+    configureMonitoring()
+}
